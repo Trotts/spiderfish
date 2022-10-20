82a67a2918f6c27682b075eb9901773ddfd022de
Cameron Trotter <c.trotter2@newcastle.ac.uk>
2022-10-20 16:46:59 +0100

    Updated spiderfish.py
    Fixing deprecated imports. Pointing scrapy to the new fishbase mirror. Removing pull of picture table info (e.g. size) as not required for my usecase (causes scrapy errors when retained).

--- fishbase/spiders/spider_fish.py
+++ fishbase/spiders/spider_fish.py
@@ -1,24 +1,26 @@
 import scrapy
 from twisted.internet import reactor
-import urlparse
+from urllib.parse import urlparse
 import re
 from fishbase.items import FishItem
 import argparse
 from pydispatch import dispatcher
 from scrapy import cmdline
 from scrapy import signals
+import unicodedata
 
 def stop_reactor():
     reactor.stop()
 
 class FishSpider(scrapy.Spider):
+        
     name = "fish"
 
     def __init__(self, family=None, *args, **kwargs):
         super(FishSpider, self).__init__(*args, **kwargs)
         dispatcher.connect(stop_reactor, signal=signals.spider_closed)
-        self.allowed_domains = ["fishbase.org"]
-        self.start_urls = ["http://www.fishbase.org/Nomenclature/FamilySearchList.php?Family="+family]
+        self.allowed_domains = ["fishbase.se"]
+        self.start_urls = ["http://www.fishbase.se/Nomenclature/FamilySearchList.php?Family="+family]
 
     def parse(self, response):
         # picks up all the links to species pages for this family
@@ -26,20 +28,19 @@ class FishSpider(scrapy.Spider):
             species_url = response.urljoin(href)
 
             # changes out PHP query with permalink to species page
-            parl = urlparse.urlparse(species_url)
+            parl = urlparse(species_url)
             parl2 = parl.scheme + "://" + parl.netloc + re.sub("SpeciesSummary.php", "", parl.path) + parl.query[3:]
-
+            
             yield scrapy.Request(parl2, self.parse_species)
 
     def parse_species(self, response):
         # get link to pictures page
-        pics = response.css('span.slabel8').xpath("a[contains(., 'Pictures')]")
+        pics = response.css('span.slabel8').xpath("a[contains(., 'Pictures')]")        
         pics_url = response.urljoin(pics.xpath("@href").extract_first())
+        
         yield scrapy.Request(pics_url, self.parse_pics)
 
     def parse_pics(self, response):
-        # thumbs = response.xpath("//span/img/@src[contains(., 'species')]").extract()
-        # species = response.xpath("//td/font/i/a/text()").extract_first()
         picpage = response.xpath("//td/a/@href[contains(., 'PicturesSummary')]").extract()
 
         for link in picpage:
@@ -47,31 +48,13 @@ class FishSpider(scrapy.Spider):
             yield scrapy.Request(link_url, self.parse_picture)
 
     def parse_picture(self, response):
-        import unicodedata
-
         table = response.xpath("//form//tr/td/text()").extract()
-        table2 = []
-        linecount = 0
-        size = 0
-
-        for line in table:
-            line = line.replace('\n', '')
-            line = line.replace('\t', '')
-            line = line.replace('\r', '')
-            line = unicodedata.normalize('NFKD', line).encode('ascii', 'ignore')
-            linecount = linecount + 1
-
-            if line=="Size (cm):":
-                size=linecount
-
-            table2.append(line)
-
-        size = table2[size]
 
         thumb = response.xpath("//img/@src[contains(., 'species')]").extract_first()
         thumb = response.urljoin(thumb)
-        species = response.xpath("//tr/td/center/i/a/text()").extract_first()
-        yield FishItem(species=species, image=[thumb], size=size)
+        species = response.xpath("//tr/td/center/a/i/text()").extract_first()
+        
+        yield FishItem(species=species, image=[thumb])
 
     def spider_closed(self, spider):
         if spider is not self:
